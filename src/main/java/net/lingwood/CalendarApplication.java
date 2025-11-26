69cc7441b0be77be144d2524c3b099b40e4bc9d5
pling <pling@192.168.160.41>
2025-11-26 15:00:51 +0000

    Token Test

--- src/main/java/net/lingwood/CalendarApplication.java
+++ src/main/java/net/lingwood/CalendarApplication.java
@@ -3,6 +3,9 @@ package net.lingwood;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+// Main Class.
+
 @SpringBootApplication
 public class CalendarApplication {
 
